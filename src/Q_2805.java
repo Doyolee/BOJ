0e88b085cff4ab836badb9114f48ddabceab90a1
Yujin Jeong <109124300+Doyolee@users.noreply.github.com>
2022-09-23 11:55:43 +0900

    나무자르기
    20220908

--- src/Q_2805.java
+++ src/Q_2805.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+ 
 public class Q_2805 {
 
 	public static void main(String[] args) throws IOException {
