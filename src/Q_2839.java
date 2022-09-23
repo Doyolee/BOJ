23a95b70b68593c4a1a42ce41d661ea0b3ea0ce1
Yujin Jeong <109124300+Doyolee@users.noreply.github.com>
2022-09-23 11:54:48 +0900

    설탕 배달 (그리드 알고리즘)
    20220922

--- src/Q_2839.java
+++ src/Q_2839.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+ 
 public class Q_2839 {
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
