f741f5a33f618bff6692b316cb84ef1b9621ac76
doyolee <panlj@naver.com>
2022-09-23 11:35:54 +0900

    20220906 알람시계

---
+++ src/Q_2884.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_2884 {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		String num[]=br.readLine().split(" ");
+		int H=Integer.parseInt(num[0]);
+		int M=Integer.parseInt(num[1]);
+		
+		if(M-45<0) {
+			M+=15;
+			if(H==0)
+				H=23;
+			else
+				H-=1;
+		}else {
+			M-=45;
+		}
+		
+		System.out.print(H+" "+M);
+
+
+	}
+
+}
