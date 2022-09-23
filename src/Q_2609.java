fa823de5e3717e7f7e3e30898bd460881fc558ce
doyolee <panlj@naver.com>
2022-09-23 11:35:02 +0900

    20220901 최대공약수와 최소공배수

---
+++ src/Q_2609.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_2609 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String num[]=br.readLine().split(" ");
+		
+		int a=Integer.parseInt(num[0]);
+		int b=Integer.parseInt(num[1]);
+		
+		int k;
+		int gcd=0;
+		int lcm=0;
+		
+		if(a>b)k=b;
+		else k=a;
+		
+		for(;k>=1;k--) {
+			if(a%k==0&&b%k==0) {
+				gcd=k;
+				break;
+			}
+		}
+		lcm=(a*b)/gcd;
+		
+		System.out.println(gcd);
+		System.out.println(lcm);
+		
+	}
+
+}
+
