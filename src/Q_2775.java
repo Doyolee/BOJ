5b8cade0999d24846cbf53907a5facf79cf10968
doyolee <panlj@naver.com>
2022-09-23 11:30:50 +0900

    20220816 부녀회장이 될테야

---
+++ src/Q_2775.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Q_2775 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int k=sc.nextInt();
+		int n=sc.nextInt();
+		
+		int apart[][]=new int[15][15];
+
+		for(int i=0;i<apart.length;i++){
+
+				for(int j=1;j<apart[i].length;j++) {
+					if(i==0)
+					{
+						apart[i][j]=j;
+					}else{
+						if(j==1)
+							apart[i][j]=apart[i-1][j];
+						else 
+							apart[i][j]=apart[i-1][j]+apart[i][j-1];
+						
+					}
+				}
+		}
+		System.out.println(apart[k][n]);
+	}
+}
+		
+		
