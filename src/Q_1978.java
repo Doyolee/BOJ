66138a6d2d5af00b5d0925448f35b2f3d5b2c6d4
doyolee <panlj@naver.com>
2022-09-23 11:34:14 +0900

    20220825 소수 찾기

---
+++ src/Q_1978.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q_1978 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		
+		int answer=0;
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		while(st.hasMoreTokens()) {
+			int n2=Integer.parseInt(st.nextToken());
+			boolean p=true;//소수는 true 아니면 false
+			
+			if(n2==1)
+				continue;
+			
+			for(int i=2;i<=Math.sqrt(n2);i++) { 
+				if(n2%i==0) {
+					p=false;
+					break;
+				}
+			}
+			if(p==true)
+				answer++;
+
+			
+		}
+		System.out.println(answer);
+	}
+
+}
