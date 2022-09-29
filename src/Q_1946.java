d0167e4f6146e1fab0e3368155ec4e4d36fefed6
Yujin Jeong <109124300+Doyolee@users.noreply.github.com>
2022-09-29 18:56:37 +0900

    신입사원
    20220926
    그리드 알고리즘
    
    1. 서류심사 순위를 인덱스, 면접 순위를 값으로 해서 배열을 생성한다.
    2. 서류심사 1위는 반드시 합격이며, 인덱스 후순위 지원자들은 전순위 지원자보다 면접 등수가 높아야만 합격할 수 있다.
       따라서 첫 합격 기준은 서류심사 1위의 면접 등수이고, 합격자가 나올 때마다 합격 기준을 갱신한다.
    3. 합격자들 중에서 가장 최근 합격자의 면접 등수가 가장 높다. 따라서 해당 지원자보다 서류심사 등수가 높은 모든 합격자와 면접 순위를 비교하는 것과 같은 과정이라고 볼 수 있다.

--- src/Q_1946.java
+++ src/Q_1946.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Q_1946 {
+public class Q_1946 { 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
